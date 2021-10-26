206f55acb1397bd48c2584ee5fb3ffe5812940ee
bradwhs <52989420+bradwhs@users.noreply.github.com>
2021-10-26 18:48:42 +0900

    Add spring-securit packages and code to protect controller endpoints

---
+++ src/main/java/jp/co/axa/apidemo/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package jp.co.axa.apidemo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+ 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+ 
+    // Set credentials for user login
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder)
+        .withUser("sa").password(passwordEncoder.encode("123456")).roles("USER");
+    }
+ 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+ 
+    // Protecting controller API endpoints, only allow access when authenticated
+    // redirect to /login page otherwise
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/api/v1/employees").authenticated()
+        .antMatchers("/api/v1/employees/**").authenticated()
+        .and().formLogin()
+        .and().logout().logoutSuccessUrl("/login").permitAll()
+        .and().csrf().disable();
+    }
+}
